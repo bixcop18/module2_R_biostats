9d71dd95a5094fb0c6ea819e09fc5b466565ef7c
Philippa Borrill <p.borrill@gmail.com>
2018-05-24 11:40:07 +0300

    Add files via upload

---
+++ Exercises/heatmap_exercises.R
@@ -0,0 +1,12 @@
+#1) plot a heatmap for the bottom 100 expressed genes in euploid shoots on chr1A
+# is the pattern similar to for the top 100 expressed genes?
+
+#2) plot a heatmap for both roots and shoots for chr1A, 
+# use the top 100 expressed genes in euploid shoots
+# add a dendrogram for the columns. You will need to make the columns re-order using colv
+# which samples are more related?
+
+#3) CHALLENGE plot the heatmap from #2) using the "aheatmap" function in NMF
+# can you add on an annotation column showing which samples are roots, and which are shoots?
+# HINT: you will need to make an annotation dataframe and pass it to annCol
+# http://nmf.r-forge.r-project.org/vignettes/heatmaps.pdf part 1.4 shows an example
